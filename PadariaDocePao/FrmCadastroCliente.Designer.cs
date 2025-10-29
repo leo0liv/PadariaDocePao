ad197f87d7190e8757a9310195386fbe0a5ba21e
Leo Oliveira <leo2281999jose@gmail.com>
2025-10-29 20:31:06 -0300

    Tela CadastroCliente Feito  e links menu

---
+++ PadariaDocePao/FrmCadastroCliente.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace PadariaDocePao
+{
+    partial class FrmCadastroCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuPadariaDocePao = new System.Windows.Forms.MenuStrip();
+            this.inícioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.matériasPrimasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.planejamentoDeProduçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuraçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputEnderecoCliente = new System.Windows.Forms.TextBox();
+            this.lbEnderecoCliente = new System.Windows.Forms.Label();
+            this.lbEstoque = new System.Windows.Forms.Label();
+            this.lbTelefoneCliente = new System.Windows.Forms.Label();
+            this.inputObsCliente = new System.Windows.Forms.TextBox();
+            this.inputTelefoneCliente = new System.Windows.Forms.TextBox();
+            this.inputNomeCliente = new System.Windows.Forms.TextBox();
+            this.lbNomeCliente = new System.Windows.Forms.Label();
+            this.pBoxPadariaDocePao = new System.Windows.Forms.PictureBox();
+            this.lbObservacao = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.btExcluir = new System.Windows.Forms.Button();
+            this.btEditarCliente = new System.Windows.Forms.Button();
+            this.menuPadariaDocePao.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPadariaDocePao)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuPadariaDocePao
+            // 
+            this.menuPadariaDocePao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(244)))), ((int)(((byte)(239)))));
+            this.menuPadariaDocePao.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.menuPadariaDocePao.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inícioToolStripMenuItem,
+            this.produtosToolStripMenuItem,
+            this.matériasPrimasToolStripMenuItem,
+            this.eToolStripMenuItem,
+            this.planejamentoDeProduçãoToolStripMenuItem,
+            this.configuraçõesToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.menuPadariaDocePao.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow;
+            this.menuPadariaDocePao.Location = new System.Drawing.Point(0, 0);
+            this.menuPadariaDocePao.Name = "menuPadariaDocePao";
+            this.menuPadariaDocePao.Size = new System.Drawing.Size(800, 23);
+            this.menuPadariaDocePao.TabIndex = 4;
+            this.menuPadariaDocePao.Text = "menuPadariaDocePao";
+            // 
+            // inícioToolStripMenuItem
+            // 
+            this.inícioToolStripMenuItem.Name = "inícioToolStripMenuItem";
+            this.inícioToolStripMenuItem.Size = new System.Drawing.Size(48, 19);
+            this.inícioToolStripMenuItem.Text = "Início";
+            this.inícioToolStripMenuItem.Click += new System.EventHandler(this.inícioToolStripMenuItem_Click);
+            // 
+            // produtosToolStripMenuItem
+            // 
+            this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(67, 19);
+            this.produtosToolStripMenuItem.Text = "Produtos";
+            this.produtosToolStripMenuItem.Click += new System.EventHandler(this.produtosToolStripMenuItem_Click);
+            // 
+            // matériasPrimasToolStripMenuItem
+            // 
+            this.matériasPrimasToolStripMenuItem.Name = "matériasPrimasToolStripMenuItem";
+            this.matériasPrimasToolStripMenuItem.Size = new System.Drawing.Size(105, 19);
+            this.matériasPrimasToolStripMenuItem.Text = "Matérias-Primas";
+            this.matériasPrimasToolStripMenuItem.Click += new System.EventHandler(this.matériasPrimasToolStripMenuItem_Click);
+            // 
+            // eToolStripMenuItem
+            // 
+            this.eToolStripMenuItem.Name = "eToolStripMenuItem";
+            this.eToolStripMenuItem.Size = new System.Drawing.Size(87, 19);
+            this.eToolStripMenuItem.Text = "Encomendas";
+            this.eToolStripMenuItem.Click += new System.EventHandler(this.eToolStripMenuItem_Click);
+            // 
+            // planejamentoDeProduçãoToolStripMenuItem
+            // 
+            this.planejamentoDeProduçãoToolStripMenuItem.Name = "planejamentoDeProduçãoToolStripMenuItem";
+            this.planejamentoDeProduçãoToolStripMenuItem.Size = new System.Drawing.Size(162, 19);
+            this.planejamentoDeProduçãoToolStripMenuItem.Text = "Planejamento de Produção";
+            // 
+            // configuraçõesToolStripMenuItem
+            // 
+            this.configuraçõesToolStripMenuItem.Name = "configuraçõesToolStripMenuItem";
+            this.configuraçõesToolStripMenuItem.Size = new System.Drawing.Size(96, 19);
+            this.configuraçõesToolStripMenuItem.Text = "Configurações";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 19);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // inputEnderecoCliente
+            // 
+            this.inputEnderecoCliente.Location = new System.Drawing.Point(137, 112);
+            this.inputEnderecoCliente.Name = "inputEnderecoCliente";
+            this.inputEnderecoCliente.Size = new System.Drawing.Size(416, 20);
+            this.inputEnderecoCliente.TabIndex = 28;
+            // 
+            // lbEnderecoCliente
+            // 
+            this.lbEnderecoCliente.AutoSize = true;
+            this.lbEnderecoCliente.Location = new System.Drawing.Point(272, 96);
+            this.lbEnderecoCliente.Name = "lbEnderecoCliente";
+            this.lbEnderecoCliente.Size = new System.Drawing.Size(134, 13);
+            this.lbEnderecoCliente.TabIndex = 27;
+            this.lbEnderecoCliente.Text = "ENDEREÇO DO CLIENTE";
+            // 
+            // lbEstoque
+            // 
+            this.lbEstoque.AutoSize = true;
+            this.lbEstoque.Location = new System.Drawing.Point(590, 46);
+            this.lbEstoque.Name = "lbEstoque";
+            this.lbEstoque.Size = new System.Drawing.Size(0, 13);
+            this.lbEstoque.TabIndex = 26;
+            // 
+            // lbTelefoneCliente
+            // 
+            this.lbTelefoneCliente.AutoSize = true;
+            this.lbTelefoneCliente.Location = new System.Drawing.Point(389, 46);
+            this.lbTelefoneCliente.Name = "lbTelefoneCliente";
+            this.lbTelefoneCliente.Size = new System.Drawing.Size(111, 13);
+            this.lbTelefoneCliente.TabIndex = 25;
+            this.lbTelefoneCliente.Text = "TELEFONE CLIENTE";
+            // 
+            // inputObsCliente
+            // 
+            this.inputObsCliente.Location = new System.Drawing.Point(559, 62);
+            this.inputObsCliente.Multiline = true;
+            this.inputObsCliente.Name = "inputObsCliente";
+            this.inputObsCliente.Size = new System.Drawing.Size(205, 70);
+            this.inputObsCliente.TabIndex = 24;
+            // 
+            // inputTelefoneCliente
+            // 
+            this.inputTelefoneCliente.Location = new System.Drawing.Point(348, 62);
+            this.inputTelefoneCliente.Name = "inputTelefoneCliente";
+            this.inputTelefoneCliente.Size = new System.Drawing.Size(205, 20);
+            this.inputTelefoneCliente.TabIndex = 23;
+            // 
+            // inputNomeCliente
+            // 
+            this.inputNomeCliente.Location = new System.Drawing.Point(137, 62);
+            this.inputNomeCliente.Name = "inputNomeCliente";
+            this.inputNomeCliente.Size = new System.Drawing.Size(205, 20);
+            this.inputNomeCliente.TabIndex = 22;
+            // 
+            // lbNomeCliente
+            // 
+            this.lbNomeCliente.AutoSize = true;
+            this.lbNomeCliente.Location = new System.Drawing.Point(185, 46);
+            this.lbNomeCliente.Name = "lbNomeCliente";
+            this.lbNomeCliente.Size = new System.Drawing.Size(106, 13);
+            this.lbNomeCliente.TabIndex = 21;
+            this.lbNomeCliente.Text = "NOME DO CLIENTE";
+            // 
+            // pBoxPadariaDocePao
+            // 
+            this.pBoxPadariaDocePao.Image = global::PadariaDocePao.Properties.Resources.Paodoce;
+            this.pBoxPadariaDocePao.Location = new System.Drawing.Point(21, 36);
+            this.pBoxPadariaDocePao.Name = "pBoxPadariaDocePao";
+            this.pBoxPadariaDocePao.Size = new System.Drawing.Size(105, 98);
+            this.pBoxPadariaDocePao.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBoxPadariaDocePao.TabIndex = 20;
+            this.pBoxPadariaDocePao.TabStop = false;
+            // 
+            // lbObservacao
+            // 
+            this.lbObservacao.AutoSize = true;
+            this.lbObservacao.Location = new System.Drawing.Point(618, 46);
+            this.lbObservacao.Name = "lbObservacao";
+            this.lbObservacao.Size = new System.Drawing.Size(87, 13);
+            this.lbObservacao.TabIndex = 29;
+            this.lbObservacao.Text = "OBSERVAÇÕES";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 156);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 221);
+            this.dataGridView1.TabIndex = 30;
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btSalvar.Location = new System.Drawing.Point(378, 383);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(133, 36);
+            this.btSalvar.TabIndex = 33;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.UseVisualStyleBackColor = false;
+            // 
+            // btExcluir
+            // 
+            this.btExcluir.BackColor = System.Drawing.Color.IndianRed;
+            this.btExcluir.Location = new System.Drawing.Point(656, 383);
+            this.btExcluir.Name = "btExcluir";
+            this.btExcluir.Size = new System.Drawing.Size(133, 36);
+            this.btExcluir.TabIndex = 32;
+            this.btExcluir.Text = "Excluir";
+            this.btExcluir.UseVisualStyleBackColor = false;
+            // 
+            // btEditarCliente
+            // 
+            this.btEditarCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btEditarCliente.Location = new System.Drawing.Point(517, 383);
+            this.btEditarCliente.Name = "btEditarCliente";
+            this.btEditarCliente.Size = new System.Drawing.Size(133, 36);
+            this.btEditarCliente.TabIndex = 31;
+            this.btEditarCliente.Text = "Editar Cliente";
+            this.btEditarCliente.UseVisualStyleBackColor = false;
+            // 
+            // FrmCadastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(244)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.btExcluir);
+            this.Controls.Add(this.btEditarCliente);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lbObservacao);
+            this.Controls.Add(this.inputEnderecoCliente);
+            this.Controls.Add(this.lbEnderecoCliente);
+            this.Controls.Add(this.lbEstoque);
+            this.Controls.Add(this.lbTelefoneCliente);
+            this.Controls.Add(this.inputObsCliente);
+            this.Controls.Add(this.inputTelefoneCliente);
+            this.Controls.Add(this.inputNomeCliente);
+            this.Controls.Add(this.lbNomeCliente);
+            this.Controls.Add(this.pBoxPadariaDocePao);
+            this.Controls.Add(this.menuPadariaDocePao);
+            this.Name = "FrmCadastroCliente";
+            this.Text = "FrmCadastroCliente";
+            this.menuPadariaDocePao.ResumeLayout(false);
+            this.menuPadariaDocePao.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPadariaDocePao)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuPadariaDocePao;
+        private System.Windows.Forms.ToolStripMenuItem inícioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matériasPrimasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem planejamentoDeProduçãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuraçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.TextBox inputEnderecoCliente;
+        private System.Windows.Forms.Label lbEnderecoCliente;
+        private System.Windows.Forms.Label lbEstoque;
+        private System.Windows.Forms.Label lbTelefoneCliente;
+        private System.Windows.Forms.TextBox inputObsCliente;
+        private System.Windows.Forms.TextBox inputTelefoneCliente;
+        private System.Windows.Forms.TextBox inputNomeCliente;
+        private System.Windows.Forms.Label lbNomeCliente;
+        private System.Windows.Forms.PictureBox pBoxPadariaDocePao;
+        private System.Windows.Forms.Label lbObservacao;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.Button btExcluir;
+        private System.Windows.Forms.Button btEditarCliente;
+    }
+}
---
+++ PadariaDocePao/FrmCadastroCliente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PadariaDocePao
+{
+    public partial class FrmCadastroCliente: Form
+    {
+        public FrmCadastroCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void inícioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmTelaPrincipal frmTelaPrincipal = new FrmTelaPrincipal();
+            this.Close();
+            frmTelaPrincipal.Show();
+        }
+
+        private void produtosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmProdutos frmProdutos = new FrmProdutos();
+            frmProdutos.Show();
+        }
+
+        private void matériasPrimasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmMateriaPrima frmMateriaPrima = new FrmMateriaPrima();
+            frmMateriaPrima.Show();
+        }
+
+        private void eToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmEncomendas frmEncomendas = new FrmEncomendas();
+            frmEncomendas.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
